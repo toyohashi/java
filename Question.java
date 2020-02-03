6408519ae95877d106671014c29ddd6574e1d40b
ncktoyohashi <42114423+toyohashi@users.noreply.github.com>
2020-02-03 20:53:27 +0900

    Add files via upload

---
+++ Question.java
@@ -0,0 +1,12 @@
+package finalprogram;
+
+public class Question {
+	String prompt;
+	String answer;
+	
+	Question(String prompt, String answer) {
+		this.prompt = prompt;
+		this.answer = answer;
+		
+	}
+}
